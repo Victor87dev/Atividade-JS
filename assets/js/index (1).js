3ea6022d6029f90ff7c3af037dbbcf33ee340963
João Victor <devsjoaovvictor@gmail.com>
2023-02-08 17:50:43 -0300

    atividade em sala de aula

---
+++ assets/js/index (1).js
@@ -0,0 +1,163 @@
+/*
+var nome = prompt('digite seu nome:');
+alert(nome + ', seja bem vindo');
+
+console.log(nome);
+
+document.write(nome);
+*/
+
+/*
+let resultado = confirm('O valor está correto ?');
+
+if(resultado==true){
+   alert('Valor corresponde');
+
+}else{
+
+    alert('Valor não corresponde');
+
+}
+*/
+
+/*
+let as = 5;
+let bs = 7;
+*/
+
+/*
+if(as > bs){
+   document.write('as é maior')
+
+}else{
+
+    document.write('as é menor')
+}
+*/
+
+/*
+if(as > bs){
+    alert('as é maior')
+}else{
+    alert('as é menor')
+}
+*/
+
+/*
+if(as > bs){
+    console.log('as é maior')
+}else{
+    console.log('as é menor')
+}
+*/
+
+/*
+let letraA = 22.50;
+let letraB = 28;
+
+if(letraA > letraB){
+    alert(letraA)
+    console.log(letraA)
+    document.write(letraA)
+}else{
+    alert(letraB)
+    console.log(letraB)
+    document.write(letraB)
+}
+*/
+
+/*
+let valor = 5;
+
+if(valor >= 0){
+    alert(valor +  " positivo");
+    document.write(valor,' positivo');
+    console.log(valor, ' positivo');
+}else{
+    alert(valor,' negativo');
+    document.write(valor,' negativo');
+    console.log(valor, ' negativo');
+}
+*/
+
+/*
+let letra = 'm'
+
+if(letra === 'm'){
+    alert('sexo: masculino')
+} else if(letra === 'f'){
+
+alert('sexo: feminino')
+
+}else{
+    alert('valor nao reconhecido')
+}
+*/
+
+/*
+let data = prompt('digite seu ano de nascimento')
+document.write("nasci no ano: "+ data)
+let idade = prompt('digite sua idade')
+document.write(" tenho: " + idade + " anos")
+*/
+
+/*
+let valor1 = prompt('digite o valor:')
+let valor2 = prompt('digite o valor:')
+
+if(valor1 > valor2){
+    document.write(valor1 + ' maior que ' + valor2)
+}else{
+    document.write(valor2 + ' maior que ' + valor1)
+}
+*/
+
+
+/*
+let nota1 = parseInt (prompt('digite a primeira nota: '))
+let nota2 = parseInt (prompt('digite a segunda nota: '))
+let nota3 = parseInt (prompt('digite a terceira nota: '))
+let nota4 = parseInt (prompt('digite a quarta nota: '))
+let nota5 = parseInt (prompt('digite a quinta nota: '))
+
+let soma = (nota1 + nota2 + nota3 + nota4 + nota5)
+
+let media = (soma/5)
+
+alert('a media das notas é: ' + media)
+
+if(media >= 7){
+    alert('aluno aprovado')
+}else if(media >= 4){
+
+    alert('aluno em recuperação')
+
+}else{
+    alert('aluno reprovado')
+}
+*/
+
+/*
+let dolar = parseFloat (prompt('digite o valor do dolar:'))
+
+
+
+let real = parseFloat (prompt('digite o valor do real:'))
+let valorConvertido = (real*dolar)
+
+alert("real convertido em dolar " + valorConvertido)
+*/
+
+/*
+let letra = 'd'
+
+switch('d'){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u': console.log('vogal');break;
+    default:
+    console.log('consoante'); break;
+}
+*/
