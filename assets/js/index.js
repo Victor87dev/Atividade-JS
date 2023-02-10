8adb7c56fd3b73c336eb6c5d3e635aa050f721bd
João Victor <devsjoaovvictor@gmail.com>
2023-02-10 18:22:27 -0300

    funções, praticando em sala de aula

---
+++ assets/js/index.js
@@ -0,0 +1,468 @@
+/*
+var nome = prompt('digite seu nome:');
+alert(nome + ', seja bem vindo');
+
+console.log(nome);
+
+document.write(nome);
+*/
+
+/*
+let resultado = confirm('O valor está correto ?');
+
+if(resultado==true){
+   alert('Valor corresponde');
+
+}else{
+
+    alert('Valor não corresponde');
+
+}
+*/
+
+/*
+let as = 5;
+let bs = 7;
+*/
+
+/*
+if(as > bs){
+   document.write('as é maior')
+
+}else{
+
+    document.write('as é menor')
+}
+*/
+
+/*
+if(as > bs){
+    alert('as é maior')
+}else{
+    alert('as é menor')
+}
+*/
+
+/*
+if(as > bs){
+    console.log('as é maior')
+}else{
+    console.log('as é menor')
+}
+*/
+
+/*
+let letraA = 22.50;
+let letraB = 28;
+
+if(letraA > letraB){
+    alert(letraA)
+    console.log(letraA)
+    document.write(letraA)
+}else{
+    alert(letraB)
+    console.log(letraB)
+    document.write(letraB)
+}
+*/
+
+/*
+let valor = 5;
+
+if(valor >= 0){
+    alert(valor +  " positivo");
+    document.write(valor,' positivo');
+    console.log(valor, ' positivo');
+}else{
+    alert(valor,' negativo');
+    document.write(valor,' negativo');
+    console.log(valor, ' negativo');
+}
+*/
+
+/*
+let letra = 'm'
+
+if(letra === 'm'){
+    alert('sexo: masculino')
+} else if(letra === 'f'){
+
+alert('sexo: feminino')
+
+}else{
+    alert('valor nao reconhecido')
+}
+*/
+
+/*
+let data = prompt('digite seu ano de nascimento')
+document.write("nasci no ano: "+ data)
+let idade = prompt('digite sua idade')
+document.write(" tenho: " + idade + " anos")
+*/
+
+/*
+let valor1 = prompt('digite o valor:')
+let valor2 = prompt('digite o valor:')
+
+if(valor1 > valor2){
+    document.write(valor1 + ' maior que ' + valor2)
+}else{
+    document.write(valor2 + ' maior que ' + valor1)
+}
+*/
+
+
+/*
+let nota1 = parseInt (prompt('digite a primeira nota: '))
+let nota2 = parseInt (prompt('digite a segunda nota: '))
+let nota3 = parseInt (prompt('digite a terceira nota: '))
+let nota4 = parseInt (prompt('digite a quarta nota: '))
+let nota5 = parseInt (prompt('digite a quinta nota: '))
+
+let soma = (nota1 + nota2 + nota3 + nota4 + nota5)
+
+let media = (soma/5)
+
+alert('a media das notas é: ' + media)
+
+if(media >= 7){
+    alert('aluno aprovado')
+}else if(media >= 4){
+
+    alert('aluno em recuperação')
+
+}else{
+    alert('aluno reprovado')
+}
+*/
+
+/*
+let dolar = parseFloat (prompt('digite o valor do dolar:'))
+
+
+
+let real = parseFloat (prompt('digite o valor do real:'))
+let valorConvertido = (real*dolar)
+
+alert("real convertido em dolar " + valorConvertido)
+*/
+
+/*
+let letra = 'd'
+
+switch('d'){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u': console.log('vogal');break;
+    default:
+    console.log('consoante'); break;
+}
+*/
+
+//objeto ---------------------------------------------
+
+/*
+let nome = 'Rafael'; //string literal
+let idade = 25; // number literal
+let estaAprovado = true; //boolean
+let sobrenome; undefined; //Undefined
+let profissao = 'dev';
+let lazer = 'academia';
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'sampaio',
+    profissao: 'dev',
+    lazer: 'academia'
+
+};
+
+console.log(pessoa);
+*/
+
+
+// arrays ------------------------------------------------
+
+/*
+let familia = [35,19,18,77];
+
+console.log(familia.length);
+
+console.log(familia[0]);
+*/
+
+/*
+let meuColega = ['Amorim',21,'solteiro','marinha'];
+
+console.log(meuColega[1]);
+*/
+
+//funcion -----------------------------------------
+
+//verbo + substantivo
+
+/*
+let corSite = "amarelo";
+function resetaCor(cor,tonalidade){
+    corSite = cor + tonalidade;
+
+};
+
+console.log(corSite);
+resetaCor("vermelho", " escuro");
+console.log(corSite);
+*/
+
+
+/*
+let minhaAcademia = "wefit";
+
+function mudarAcademia(acad, local){
+
+    minhaAcademia = acad + local;
+
+};
+
+console.log(minhaAcademia);
+mudarAcademia("green", " jordao baixo");
+console.log(minhaAcademia);
+*/
+
+// rocketseat -------------------------------------------
+
+/*
+var weight 
+
+let name = 'jose'
+let abi = 18
+let stars = 18.1
+let isSubscribed = true  
+*/
+
+/*
+let student = {
+
+};
+
+console.log(typeof student)
+*/
+
+
+
+
+
+/*
+console.log(`${student.name} tem ${student.idade} e pesa ${student.star} kg ` )
+
+
+console.log(student.name + ' tem ' + student.idade + ' e pesa ' + student.star + ' kg ')
+*/
+
+/*
+
+let student = {
+    name: 'joao',
+    idade: 20,
+    peso: 10.4
+}
+
+let students = [
+    student
+]
+
+
+let mark = {
+    name: 'rock',
+    idade:15,
+    peso:48.5
+}
+
+
+students[1] = mark
+
+console.log(students)
+
+*/
+
+/*
+
+let id = (prompt(" entre com sua idade "))
+
+let ano1 = 2023
+
+let result = IdadeAtual(id,ano1)
+
+let resultado2 = Idade(result,ano1)
+
+function IdadeAtual(){
+
+    return ano1 - id 
+} 
+
+alert(" o seu ano de nascimento é: " + result + " e sua idade é: " + resultado2)
+
+function Idade(){
+    return ano1 - result
+}
+
+*/
+
+/*
+
+let valor1 = (prompt("entre com um numero:"))
+let valor2 = (prompt("entre com um segundo numero:"))
+
+alert("primeiro numero: " + valor1 + ", segundo numero : " + valor2)
+console.log( "primeiro numero: " + valor1 + ", segundo numero : " + valor2)
+document.write( "primeiro numero: " + valor1 + ", segundo numero : " + valor2)
+
+*/
+
+
+/*
+ let salarioBase = parseFloat(prompt("entre com o salario base: "))
+ const gratificacao = 0.05;
+ const imposto = 0.07
+
+ let resultadoS = gratificacaoEsalario(salarioBase,gratificacao)
+
+ function gratificacaoEsalario (){
+     return salarioBase * gratificacao
+ }
+
+
+let resultadoFinal = (resultadoS / imposto) 
+
+ document.write("o salario final é: " + resultadoFinal)
+*/
+
+/*
+
+ 
+ let meuNome = (prompt("entre com seu nome: "))
+ let suaIdade = (prompt("entre com sua idade: "))
+
+ alert("seu nome é: " + meuNome + " e " + " sua idade é: " + suaIdade)
+ console.log("seu nome é: " + meuNome + " e " + " sua idade é: " + suaIdade)
+ document.write("seu nome é: " + meuNome + " e " + " sua idade é: " + suaIdade)
+*/
+
+/*
+let numero1 =  parseFloat(prompt("digite o primeiro numero : "))
+let numero2 = parseFloat (prompt("digite o segundo numero: "))
+
+let soma = (numero1 + numero2)
+let subtracao = (numero1 - numero2)
+let multiplicacao = (numero1 * numero2)
+let divisao = (numero1 / numero2)
+
+document.write(`a soma: ${soma}, a subtração: ${subtracao}, a multiplicaçã: ${multiplicacao}, a divisão: ${divisao}`)
+
+*/
+
+/*
+let numero1 =  parseFloat(prompt("digite o primeiro numero : "))
+let numero2 = parseFloat (prompt("digite o segundo numero: "))
+
+let somaDosNumeros = somaFuncao(numero1,numero2)
+
+function somaFuncao (){
+    return numero1 + numero2
+}
+
+
+let subtracaoDosNumeros = subtracaoFuncao(numero1,numero2)
+
+function subtracaoFuncao (){
+    return numero1 - numero2
+}
+
+
+let divisaoDosNumeros = divisaoFuncao(numero1,numero2)
+
+function divisaoFuncao (){
+    return numero1 / numero2
+}
+
+
+let multiplicacaoDosNumeros = multiplicacaoFuncao(numero1,numero2)
+
+function multiplicacaoFuncao () {
+
+    return numero1 * numero2
+}
+
+document.write(` soma função: ${somaDosNumeros} , subtração função: ${subtracaoDosNumeros} , divisão função: ${divisaoDosNumeros} , multiplicação função: ${multiplicacaoDosNumeros}`)
+alert(`soma função: ${somaDosNumeros} , subtração função: ${subtracaoDosNumeros} , divisão função: ${divisaoDosNumeros} , multiplicação função: ${multiplicacaoDosNumeros}`) 
+console.log(`soma função: ${somaDosNumeros} , subtração função: ${subtracaoDosNumeros} , divisão função: ${divisaoDosNumeros} , multiplicação função: ${multiplicacaoDosNumeros}`)  
+
+*/
+
+/*
+let nome = (prompt("digite o seu nome: "))
+let endereco = (prompt("digite sua cidade, estado e cep: "))
+let telefone = (prompt("digite seu telefone: "))
+let profissao = (prompt("digite sua profissao: "))
+
+document.write(`seu nome: ${nome} , seu endereço: ${endereco} , seu telefone: ${telefone} , sua profissao: ${profissao}`)
+*/
+
+/*
+let valorDeVendas = parseFloat (prompt("digite o valor total de vendas: "))
+const lucro = 0.23
+
+let lucroDaEmpresa = calculoPorcentagem(valorDeVendas,lucro)
+
+function calculoPorcentagem (){
+    return valorDeVendas * lucro
+}
+
+document.write(`o lucro da empresa é: ${lucroDaEmpresa}`)
+*/
+
+/*
+let numero1 = parseInt (prompt("digite o primeiro numero: " ))
+let numero2 = parseInt (prompt("digite o segundo numero: " ))
+let numero3 = parseInt (prompt("digite o terceiro numero: " ))
+let numero4 = parseInt (prompt("digite o quarto numero: " ))
+let numero5 = parseInt (prompt("digite o quinto numero: " ))
+
+let mais = Math.max (numero1,numero2,numero3,numero4,numero5)
+
+document.write(mais)
+*/
+
+//-------------------------------------------------------------
+/*
+let idade1 = parseInt (prompt("digite a primeira idade: " ))
+let idade2 = parseInt (prompt("digite a segunda idade: " ))
+let idade3 = parseInt (prompt("digite a terceira idade: " ))
+
+if(idade1 >= 18){
+
+    alert(idade1)
+
+*/
+//-----------------------------------------------------------------
+
+/*
+let peso = parseFloat (prompt("informe o seu peso: "))
+let altura = parseFloat (prompt("informe a sua altura: "))
+let idade = parseFloat (prompt("informe a sua idade: "))
+let nome = (prompt("informe o seu nome: "))
+
+let resultadoIMC = calcularImc (peso,altura)
+
+function calcularImc (){
+
+    return peso * altura 
+}
+
+document.write(`seu imc é: ${resultadoIMC}` )
+
+*/
