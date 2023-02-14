82dc08880170913fc0fb0a7777cea81565483c1c
João Victor <devsjoaovvictor@gmail.com>
2023-02-14 19:32:45 -0300

    folder function

---
+++ assets/js/function.js
@@ -0,0 +1,125 @@
+// --------------------- function------------------------------
+
+//verbo + substantivo
+
+// atividade 1 ------------------------------------------------
+
+/*
+let corSite = "amarelo";
+function resetaCor(cor,tonalidade){
+    corSite = cor + tonalidade;
+
+};
+
+console.log(corSite);
+resetaCor("vermelho", " escuro");
+console.log(corSite);
+*/
+
+// ativiade 2 -------------------------------------------------
+
+
+/*
+let minhaAcademia = "wefit";
+
+function mudarAcademia(acad, local){
+
+    minhaAcademia = acad + local;
+
+};
+
+console.log(minhaAcademia);
+mudarAcademia("green", " jordao baixo");
+console.log(minhaAcademia);
+*/
+
+// atividade 3 -----------------------------------------
+
+
+/*
+
+let id = (prompt(" entre com sua idade "))
+
+let ano1 = 2023
+
+let result = IdadeAtual(id,ano1)
+
+let resultado2 = Idade(result,ano1)
+
+function IdadeAtual(){
+
+    return ano1 - id 
+} 
+
+alert(" o seu ano de nascimento é: " + result + " e sua idade é: " + resultado2)
+
+function Idade(){
+    return ano1 - result
+}
+
+*/
+
+
+// atividade 4 -------------------------------------------------
+
+
+/*
+ let salarioBase = parseFloat(prompt("entre com o salario base: "))
+ const gratificacao = 0.05;
+ const imposto = 0.07
+
+ let resultadoS = gratificacaoEsalario(salarioBase,gratificacao)
+
+ function gratificacaoEsalario (){
+     return salarioBase * gratificacao
+ }
+
+
+let resultadoFinal = (resultadoS / imposto) 
+
+ document.write("o salario final é: " + resultadoFinal)
+
+*/
+
+// ativiade 5 --------------------------------------------
+
+/*
+let numero1 =  parseFloat(prompt("digite o primeiro numero : "))
+let numero2 = parseFloat (prompt("digite o segundo numero: "))
+
+let somaDosNumeros = somaFuncao(numero1,numero2)
+
+function somaFuncao (){
+    return numero1 + numero2
+}
+
+
+let subtracaoDosNumeros = subtracaoFuncao(numero1,numero2)
+
+function subtracaoFuncao (){
+    return numero1 - numero2
+}
+
+
+let divisaoDosNumeros = divisaoFuncao(numero1,numero2)
+
+function divisaoFuncao (){
+    return numero1 / numero2
+}
+
+
+let multiplicacaoDosNumeros = multiplicacaoFuncao(numero1,numero2)
+
+function multiplicacaoFuncao () {
+
+    return numero1 * numero2
+}
+
+document.write(` soma função: ${somaDosNumeros} , subtração função: ${subtracaoDosNumeros} , divisão função: ${divisaoDosNumeros} , multiplicação função: ${multiplicacaoDosNumeros}`)
+alert(`soma função: ${somaDosNumeros} , subtração função: ${subtracaoDosNumeros} , divisão função: ${divisaoDosNumeros} , multiplicação função: ${multiplicacaoDosNumeros}`) 
+console.log(`soma função: ${somaDosNumeros} , subtração função: ${subtracaoDosNumeros} , divisão função: ${divisaoDosNumeros} , multiplicação função: ${multiplicacaoDosNumeros}`)  
+
+*/
+
+
+
