ee0bc031331452525b1963d4dab2ff53984029cd
João Victor <devsjoaovvictor@gmail.com>
2023-02-17 11:05:43 -0300

    exercise 6

---
+++ assets/exercises_course/exercise 6/forms.js
@@ -0,0 +1,60 @@
+function mesDeReferencia(){
+
+    let mes = (document.getElementById("mesResposta").value)
+
+if(mes === '1'){
+
+        document.getElementById("resposta1").innerHTML = "seu mês é: janeiro"
+
+
+}else if(mes === '2'){
+
+        document.getElementById("resposta1").innerHTML = "seu mês é: fevereiro"
+        
+}else if(mes === '3'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: março"
+    
+}else if(mes === '4'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: abril"
+    
+}else if(mes === '5'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: maio"
+    
+}else if(mes === '6'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: junho"
+    
+}else if(mes === '7'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: julho"
+    
+}else if(mes === '8'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: agosto"
+    
+}else if(mes === '9'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: setembro"
+    
+}else if(mes === '10'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: outubro"
+    
+}else if(mes === '11'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: novembro"
+    
+}else if(mes === '12'){
+
+    document.getElementById("resposta1").innerHTML = "seu mês é: dezembro"
+    
+}else{
+
+    document.getElementById("resposta1").innerHTML = "seu mês é invalido"
+    
+}
+
+}
