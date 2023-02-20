a44a947e974999b056c3079dcadcc488e038b4aa
João Victor <devsjoaovvictor@gmail.com>
2023-02-20 13:57:06 -0300

    exercise 13

---
+++ assets/exercises_course/exercise 13/forms.js
@@ -0,0 +1,28 @@
+function numeroDivisivelPor3(){
+
+let number = parseInt (document.getElementById("numero").value)
+
+
+
+
+
+
+
+
+if(number % 3 == 0){
+
+    document.getElementById("resposta").innerHTML = true
+
+
+
+}else{
+
+
+    document.getElementById("resposta").innerHTML = false
+
+}
+
+
+
+
+}
