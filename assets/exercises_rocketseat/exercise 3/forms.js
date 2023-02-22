50fdcb38f31690080e014803bb160581660fe4dc
João Victor <devsjoaovvictor@gmail.com>
2023-02-22 13:43:44 -0300

    exercise way to solve rocketseat

--- assets/exercises_rocketseat/exercise 3/forms.js
+++ assets/exercises_rocketseat/exercise 3/forms.js
@@ -12,7 +12,7 @@ function transformFc(fahrenheit){
 
 }
 
-console.log("valor em Celsius: " + transformFc(50))
+console.log("temperatura em Celsius: " + transformFc(50))
 */
 
 
@@ -28,7 +28,7 @@ console.log("celsius: " + celsius())
 
 
 // celsius -> farenheit ---------------------
-
+/*
 function fahrenheit(){
 
     let celsius = (prompt("digite celsius: "))
@@ -36,3 +36,53 @@ function fahrenheit(){
 }
 
 console.log("fahrenheit: " + fahrenheit())
+*/
+
+
+// rocketseat -----------------------------------
+
+function transformDegree(degree){
+
+    const celsiusExists = degree.toUpperCase().includes('C')
+    const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    //fluxo de erro 
+
+    if(!celsiusExists && !fahrenheitExists){
+     
+        throw new Error('Grau não identificado')
+        
+
+    }
+
+    // fluxo ideal, F -> C 
+
+    let updatedDegree = Number(degree.toUpperCase().replace("F", ""));
+    let formula = fahrenheit => (fahrenheit - 32) * 5/9
+    let degreeSign = 'C'
+
+    // fluxo alternativo C -> F
+
+    if(celsiusExists){
+
+         updatedDegree = Number(degree.toUpperCase().replace("C", ""));
+         formula = celsius => celsius * 9/5 + 32
+         degreeSign = 'F'
+
+    }
+
+return formula(updatedDegree) + degreeSign
+
+
+
+}
+
+try{
+ console.log(transformDegree('10C'))
+ console.log(transformDegree('50F'))
+ transformDegree('50Z')
+} catch(error){
+
+    console.log(error.message)
+
+}
