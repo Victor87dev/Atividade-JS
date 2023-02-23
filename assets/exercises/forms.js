7000d01fd420ab8a54f097de75feb8202fafcb4f
João Victor <devsjoaovvictor@gmail.com>
2023-02-23 11:40:17 -0300

    exercise 1 training programming logic

---
+++ assets/exercises/forms.js
@@ -0,0 +1,56 @@
+let musica = (prompt("Digite a música que você deseja ouvir: "))
+
+switch(musica){
+
+    case 'ficha':
+        console.log('Você selecionou a música: FICHA LIMPA - Gusttavo Lima. ');
+    break ;
+    case 'amor':
+    console.log('Você selecionou a música: SE FOR AMOR - João gomes e vitor. ');
+    break;
+    case 'chega':
+        console.log('Você selecionou a música: CHEGA E SENTA - John. ');
+    break;
+    case 'arranhao':
+        console.log('Você selecionou a música: ARRANHAO - Henrique e Juliano. ');
+    break;
+    case 'volta':
+        console.log('Você selecionou a música: VOLTA COMIGO BB - Ze Vaqueiro. ');
+    break;
+    default : 
+    console.log('sua música não foi encontrada.');
+    break;
+
+
+
+}
+
+let musicas = (prompt("Digite a segunda música que você deseja ouvir: "))
+
+let ficha = "Você selecionou a música: FICHA LIMPA - Gusttavo Lima. "
+let amor = "Você selecionou a música: FICHA LIMPA - Gusttavo Lima. "
+let chega = "Você selecionou a música: CHEGA E SENTA - John. "
+let arranhao = "Você selecionou a música: ARRANHAO - Henrique e Juliano. "
+let volta = "Você selecionou a música: VOLTA COMIGO BB - Ze Vaqueiro. "
+
+if(musicas === 'amor'){
+
+    console.log(amor)
+
+}else if(musicas === 'ficha'){
+ 
+    console.log(ficha)
+}else if(musicas === 'chega'){
+ 
+    console.log(chega)
+}else if(musicas === 'arranhao'){
+ 
+    console.log(arranhao)
+}else if(musicas === 'volta'){
+ 
+    console.log(volta)
+}else{
+
+console.log('Música não encontrada.')
+
+}
