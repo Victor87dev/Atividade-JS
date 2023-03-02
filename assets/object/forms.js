c2793d62de7720765200f035496173de38a32721
João Victor <devsjoaovvictor@gmail.com>
2023-03-01 22:44:51 -0300

    object page

---
+++ assets/object/forms.js
@@ -0,0 +1,129 @@
+//objeto ---------------------------------------------
+
+/*
+let nome = 'Rafael'; //string literal
+let idade = 25; // number literal
+let estaAprovado = true; //boolean
+let sobrenome; undefined; //Undefined
+let profissao = 'dev';
+let lazer = 'academia';
+
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'sampaio',
+    profissao: 'dev',
+    lazer: 'academia'
+
+};
+
+console.log(pessoa);
+*/
+
+//-------------------------------------------------
+
+/*
+var weight 
+
+let name = 'jose'
+let abi = 18
+let stars = 18.1
+let isSubscribed = true  
+*/
+
+/*
+let student = {
+
+};
+
+console.log(typeof student)
+*/
+
+
+
+
+
+/*
+console.log(`${student.name} tem ${student.idade} e pesa ${student.star} kg ` )
+
+
+console.log(student.name + ' tem ' + student.idade + ' e pesa ' + student.star + ' kg ')
+*/
+
+/*
+
+let student = {
+    name: 'joao',
+    idade: 20,
+    peso: 10.4
+}
+
+let students = [
+    student
+]
+
+
+let mark = {
+    name: 'rock',
+    idade:15,
+    peso:48.5
+}
+
+
+students[1] = mark
+
+console.log(students)
+
+*/
+
+
+//----------------------------------------
+
+
+// 2 modos de crir objetos 
+
+/*
+
+modo 1 usando 'new'
+
+let name = new String('mayk')
+let age = new Number(23)
+
+console.log(name, age)
+*/
+
+/*
+const person = {
+
+    name: 'mayk',
+    age: 25
+
+
+}
+
+console.log(person)
+*/
+
+// delete person.age
+
+//-----------------------------------------------------
+
+// for-in --------------------------
+// object 
+/*
+let person = {
+
+    name: 'john',
+    age: 30,
+    weight: 88.6
+
+}
+
+for(let property in person){
+console.log(property)
+console.log(person[property])
+
+}
+*/
+
