5d8d4fac599c15923b29511c46406e0f544eeae6
João Victor <devsjoaovvictor@gmail.com>
2023-03-02 21:25:04 -0300

    exercise 5 my form of resolution

--- assets/exercises/exercise 5/forms.js
+++ assets/exercises/exercise 5/forms.js
@@ -1,50 +1,42 @@
-
-
-
-/*
-let listOfSuspects = []
-
-let prohibited = (prompt(`Digite a entrada`))
-
-switch(prohibited){
-  case `novo suspeito - altissima periculosidade`:
-  listOfSuspects.push('joao')
-  break;
-  case `novo suspeito - pouco perigoso`:
-  listOfSuspects.push('marcos')
-  break;
-  default:
-  `nada`
-  break
-
-}
-
-console.log(listOfSuspects)
-*/
+function mudarPosicao (arr, from, to) {
+ arr.splice(to, 0, arr.splice(from, 1)[0]);
+ return arr;
+} 
 
 let listOfSuspects = []
 
 listOfSuspects[0] = (prompt(`novo suspeito - altissima periculosidade`))
 listOfSuspects[1] = (prompt(`novo suspeito - pouco perigoso`))
 listOfSuspects[2] = (prompt(`livre de suspeita, pode remover`))
+listOfSuspects[3] = (prompt(`que estranho, esses dois meliantes.. troque-os de lugar`))
+listOfSuspects[4] = (prompt(`que estranho, esses dois meliantes.. troque-os de lugar`))
 
 console.log(`novo suspeito - altissima periculosidade
- ${listOfSuspects[0]}   
- novo suspeito - pouco perigoso 
- ${listOfSuspects[1]}
- livre de suspeita, pode remover
- ${listOfSuspects[2]} 
- `)
- listOfSuspects.pop()
- 
+${listOfSuspects[0]}  
 
-function mudarPosicao(arr, from, to){
-  arr.splice(to, 0, arr.splice(from, 1)[0]);
-  return arr;
-} 
+novo suspeito - pouco perigoso 
+${listOfSuspects[1]}
 
-mudarPosicao(listOfSuspects, 0,1)
+livre de suspeita, pode remover
+${listOfSuspects[2]} 
+
+que estranho, esses dois meliantes.. troque-os de lugar
+${listOfSuspects[3]}
+${listOfSuspects[4]}
+
+essa posição não esta de acordo, ele não é tao perigoso assim
+${listOfSuspects[1]}
+
+como a lista esta ficando ?
+${listOfSuspects}
+`)
+
+mudarPosicao(listOfSuspects, 1,2)
+mudarPosicao(listOfSuspects, 3,4)
+listOfSuspects.push(listOfSuspects.splice(0,1)[0])
+
 console.log(listOfSuspects)
-
+console.log(`ja temos nossa lista de suspeitos: 
+suspeito 1 ${listOfSuspects[0]} suspeito 2 ${listOfSuspects[1]} suspeito 3 ${listOfSuspects[2]} suspeito 4 ${listOfSuspects[3]} suspeito 5 ${listOfSuspects[4]}`)
 
 
