ec1b0b73cb71051d7ab586b632820c04ca5a3a2b
João Victor <devsjoaovvictor@gmail.com>
2023-03-08 23:25:28 -0300

    exercises course click events

--- assets/exercises_course/exercise 16/forms.js
+++ assets/exercises_course/exercise 16/forms.js
@@ -161,3 +161,4 @@ numeros.sort(function(a,b) {
   
 console.log(numeros)
 */  
+
---
+++ assets/exercises_course/exercise 17/forms.js
@@ -0,0 +1,79 @@
+/*
+let botao = document.getElementById("meu-botao");
+
+botao.addEventListener("click", function() {
+
+    console.log("Botão clicado!");
+});
+
+const exemplo1 = document.querySelector("#exemplo1");
+
+exemplo1.addEventListener("mouseover", () => {
+  exemplo1.innerHTML = "o mouse esta em cima";
+});
+
+exemplo1.addEventListener("mouseout", () => {
+
+    exemplo1.innerHTML = "passse o mouse aqui";
+
+});
+
+const exemplo2 = document.querySelector("#exemplo2");
+
+exemplo2.addEventListener("mousemove", (event) => {
+    exemplo2.style.backgroundColor = "rgb(" + event.offsetX + "," + event.offsetY + ",100)";
+
+});
+*/
+
+/*
+const form = document.querySelector('#meu-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // previne o comportamento padrão de enviar o formulario 
+    const nome = document.querySelector('#nome').value;
+    const email = document.querySelector('#email').value;
+    console.log(`o nome é ${nome} e o e-mail é ${email}`);
+    form.reset();// limpa os campos do formulario
+
+});
+
+form.addEventListener('reset', () => {
+    console.log('Formulario resetado!');
+});
+
+const inputNome = document.querySelector('#nome');
+inputNome.addEventListener('focus', () => {
+  console.log('o campo nome recebeu o foco!');    
+});
+
+inputNome.addEventListener('blur', () => {
+    console.log('o campo nome perdeu o foco!');
+
+});
+*/
+
+/*
+let botao1 = document.getElementById("botao1");
+
+botao1.addEventListener("click", function() {
+
+   alert("Botao1 clicado!");
+});
+
+let botao2 = document.getElementById("botao2");
+
+botao2.addEventListener("click", function() {
+
+    alert("botao2 clicado!");
+});
+*/
+
+/*
+const form = document.querySelector("#texto")
+
+form.addEventListener("submit", (event) => {
+   const nome = document.querySelector("#nome").value;
+   alert(`o seu nome é ${nome}`)
+})
+*/
