21056eb378fbb47ff066c37ba390f74fa74b263a
João Victor <devsjoaovvictor@gmail.com>
2023-05-11 14:00:09 -0300

    classes no POO

--- assets/objetos_cfbCursos/forms.js
+++ assets/objetos_cfbCursos/forms.js
@@ -261,6 +261,8 @@ btn_add.addEventListener("click",(evt)=>{
 
 // o mesmo exemplo usando objetos literais e usando for in para percorrer o objeto 
 
+/*
+
 const Pessoa={
   nome:"",
   idade:0,
@@ -296,3 +298,103 @@ btn_add.addEventListener("click",(evt)=>{
   Pessoa.setIdade(idade.value)
   addPessoa()
 })
+
+*/
+
+/*
+
+// herança na POO ---------------------------------
+
+class Carro { // Classe pai / classe base
+  constructor(nome,portas){
+    this.nome = nome 
+    this.portas = portas 
+    this.ligado = false 
+    this.vel = 0
+    this.cor = undefined
+  }
+  ligar=function(){
+     this.ligado = true
+  }
+  desligar=function(){
+    this.ligado = false 
+  }
+  setCor=function(cor){
+    this.cor = cor 
+  }
+}
+
+class Militar extends Carro { // classe filho
+  constructor(nome,portas,blindagem,municao){
+    super(nome,portas)
+    this.blindagem = blindagem
+    this.municao = municao 
+    this.setCor("verde")
+  }
+  atirar=function(){
+    if(this.municao>0){
+      this.municao--
+    }
+  }
+}
+
+class Utilitario extends Carro {
+  constructor(nome,portas,lugares,ipva){
+    super(nome,portas)
+    this.lugares = lugares
+    this.ipva = ipva
+  }
+  setVel=function(vel){
+    this.vel = vel
+  }
+}
+
+const c1 = new Carro("normal",4)
+c1.ligar()
+c1.setCor("preto")
+
+const c2 = new Militar("lutador",1,100,50)
+c2.setCor("azul")
+c2.atirar()
+c2.atirar()
+c2.atirar()
+c2.atirar()
+
+const c3 = new Utilitario("maximi",3,"maceio",1500)
+c3.setCor("branco")
+c3.desligar()
+c3.setVel(500)
+
+console.log(`Nome: ${c1.nome}`)
+console.log(`Portas: ${c1.portas}`)
+console.log(`Ligado: ${c1.ligado?"sim":"nao"}`)
+console.log(`Velocidade: ${c1.vel}`)
+console.log(`Cor: ${c1.cor}`)
+console.log("------------------------")
+
+console.log(`Nome: ${c2.nome}`)
+console.log(`Portas: ${c2.portas}`)
+console.log(`Ligado: ${c2.ligado?"sim":"nao"}`)
+console.log(`Velocidade: ${c2.vel}`)
+console.log(`Cor: ${c2.cor}`)
+console.log(`Blindagem: ${c2.blindagem}`)
+console.log(`Cor: ${c2.municao}`)
+console.log("------------------------")
+
+console.log(`Nome: ${c3.nome}`)
+console.log(`Portas: ${c3.portas}`)
+console.log(`Lugares: ${c3.lugares}`)
+console.log(`Cor: ${c3.cor}`)
+console.log(`Ligado: ${c3.ligado?"sim":"nao"}`)
+console.log(`Ipva: ${c3.ipva}`)
+console.log(`Velocidade: ${c3.vel}`)
+console.log("------------------------")
+
+*/
+
+// exercicio de classes, POO 
+
+
+
+
+
